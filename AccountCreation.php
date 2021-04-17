d0fdcc545aea3761a8a10856631a6bf97ef47b91
Rakan Kandah <rakankandah@gmail.com>
2021-04-16 22:58:33 -0700

    Retailer registration

--- AccountCreation.php
+++ AccountCreation.php
@@ -2,16 +2,17 @@
     <head><title>Submission</title></head>
     <body>
         <?php  
-            $first_name = "ROCKY";
+            $first_name = $_POST["first_name"];
             $last_name = $_POST["last_name"];
             $password_encrypted = password_hash($_POST["password_encrypted"],PASSWORD_DEFAULT);
             $phone = $_POST["phone"];
             $email = $_POST["email"];
+            $retailer_id = $_POST["retailer_id"];
             $conn = mysqli_connect("sql3.freesqldatabase.com", "sql3402886", "gn4yJmWUfg","sql3402886");
             if (!$conn) {  
                 die("Connection failed: " . mysqli_connect_error());
             }
-            $sql = "INSERT INTO customer (email, first_name, last_name, password_encrypted, phone) VALUES ('$email','$first_name','$last_name','$password_encrypted','$phone')";
+            $sql = "INSERT INTO customer (email, first_name, last_name, password_encrypted, phone, retailer_id) VALUES ('$email','$first_name','$last_name','$password_encrypted','$phone', '$retailer_id')";
             $results = mysqli_query($conn, $sql);
             if ($results)
                 echo "Great! You're registered.";
@@ -22,6 +23,6 @@
             }
         ?>      
     </br>
-    <a href="Registration.html">Login</a>
+    <a href="Registration.html">You're information was saved! Login</a>
     </body>
 </html>
