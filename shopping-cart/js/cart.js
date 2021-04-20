f59241c956a60cc93788d4a46d5c554475979337
Paul Z <crowbargel@gmail.com>
2021-04-19 21:29:45 -0700

    Shopping cart module

---
+++ shopping-cart/js/cart.js
@@ -0,0 +1,163 @@
+$(document).ready(function() {
+    updateEmptyCartMsg();
+    $('.remove').click(function() {
+        var item = this;
+        var idToRemove = $(item).data('id');
+
+        var confirmMsg = confirm("Remove item?");
+        if (confirmMsg) {
+            $.ajax({
+                url: '../src/modify_cart.php',
+                type: 'POST',
+                data: { 
+                    action: 'remove',
+                    id: idToRemove
+                },
+                success: function(response) {
+                    if (response == 1) {
+                        var imgId = '#img-' + idToRemove;
+                        var rowId = '#item-row-' + idToRemove;
+                        $(imgId).find('img')
+                            .css({'transform': 'scale(0)'})
+                            .promise().done(function() {
+                                $(rowId).slideUp("slow", function() {
+                                    updateEmptyCartMsg();
+                                });
+                        });
+                        updateSubtotal();
+                        updateCartItemCount();
+                    }
+                    else {
+                        alert('Failed to remove item!');
+                    }
+                }
+            });
+        }
+    });
+
+    $('.decrement').click(function() {
+        var idToDecrement = $(this).data('id');
+        var textId = 'quantity-text-' + idToDecrement;
+        var newVal = Number(document.getElementById(textId).innerHTML) - 1;
+        if (newVal < 1) {
+            return;
+        }
+        
+        $.ajax({
+            url: '../src/modify_cart.php',
+            type: 'POST',
+            data: { 
+                action: 'decrement',
+                id: idToDecrement,
+                newVal: newVal
+            },
+            success: function(response) {
+                if (response == 1) {
+                    document.getElementById(textId).innerHTML = newVal;
+                    updateItemTotal(idToDecrement);
+                    updateSubtotal();
+                    updateCartItemCount();
+                }
+                else {
+                    alert('Failed to decrement!');
+                }
+            }
+        });
+    });
+
+    $('.increment').click(function() {
+        var idToIncrement = $(this).data('id');
+        var textId = 'quantity-text-' + idToIncrement;
+        var newVal = Number(document.getElementById(textId).innerHTML) + 1;
+        if (newVal > 99) {
+            return;
+        }
+
+        $.ajax({
+            url: '../src/modify_cart.php',
+            type: 'POST',
+            data: { 
+                action: 'increment',
+                id: idToIncrement,
+                newVal: newVal
+            },
+            success: function(response) {
+                if (response == 1) {
+                    document.getElementById(textId).innerHTML = newVal;
+                    updateItemTotal(idToIncrement);
+                    updateSubtotal();
+                    updateCartItemCount();
+                }
+                else {
+                    alert('Failed to increment!');
+                }
+            }
+        });
+    });
+});
+
+function updateItemTotal(id) {
+    var quantity = Number(document.getElementById('quantity-text-' + id).innerHTML);
+    var price = 
+        Number((document.getElementById('item-price-text-' + id).innerHTML).substring(1));
+    var newTotal = quantity * price;
+    document.getElementById('item-total-text-' + id).innerHTML = '$' + newTotal.toFixed(2);
+}
+
+// Get current subtotal
+function updateSubtotal() {
+    $.ajax({
+        url: '../src/modify_cart.php',
+        type: 'POST',
+        data: { 
+            action: 'getSubtotal',
+            id: '0'
+        },
+        success: function(response) {
+            document.getElementById('subtotal-text').innerHTML = 'Subtotal: $' + response;
+        }
+    });
+}
+
+// Update current cart item count
+function updateCartItemCount() {
+    $.ajax({
+        url: '../src/modify_cart.php',
+        type: 'POST',
+        data: { 
+            action: 'getCartItemCount',
+            id: '0'
+        },
+        success: function(response) {
+            document.getElementById('cart-item-count-text').innerHTML = response;
+            if (response == 0 || response == 1) {
+                document.getElementById('summary-item-count-text').innerHTML = response + ' item';
+            }
+            else {
+                document.getElementById('summary-item-count-text').innerHTML = response + ' items';
+            }
+        }
+    });
+}
+
+// Show or hide empty cart msg
+function updateEmptyCartMsg() {
+    $.ajax({
+        url: '../src/modify_cart.php',
+        type: 'POST',
+        data: { 
+            action: 'getCartItemCount',
+            id: '0'
+        },
+        success: function(response) {
+            if (response == 0) {
+                document.getElementById('empty-msg-row').style.display = 'flex';
+                document.getElementById('check-out-btn').disabled = true;
+            }
+            else {
+                document.getElementById('empty-msg-row').style.display = 'none';
+                document.getElementById('check-out-btn').disabled = false;
+            }
+        }
+    });
+}
--- shopping-cart/src/cart.php
+++ shopping-cart/src/cart.php
@@ -1,79 +1,209 @@
 <?php 
+session_start();
+include 'config.php';
+$pdo = pdo_connect_mysql();
 
-    session_start();
-    include 'function.php';
-    $pdo = pdo_connect_mysql();
+$_SESSION['user_id'] = 1;
 
-    // Routing
-    // $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
-    // include $page . '.php';
+$user_id = $_SESSION['user_id'];
 
-    $cart_id = 1;
+// Fetch cart id
+$stmt = $pdo->prepare("SELECT cart_id FROM shopping_cart WHERE FK_customer_id = '$user_id'");
+$stmt->execute();
+$cart_id = $stmt->fetchColumn();
 
-    // Fetch items in cart using the cart id
-    $stmt = $pdo->prepare("SELECT * FROM item_in_cart WHERE FK_cart_id = '$cart_id'");
-    $stmt->execute();
-    $items_in_cart = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Fetch product information that are in cart
-    $stmt = $pdo->prepare("SELECT product.* FROM item_in_cart, product WHERE product.product_id = item_in_cart.FK_product_id AND item_in_cart.FK_cart_id = '$cart_id'");
-    $stmt->execute();
-    $product_info = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Fetch items in cart using the cart id
+$stmt = $pdo->prepare("SELECT * FROM item_in_cart WHERE FK_cart_id = '$cart_id'");
+$stmt->execute();
+$items_in_cart = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $subtotal = 0.00;
-    foreach ($items_in_cart as $item) {
-        echo 'Q: ' . $item['quantity'] . ' FK: '. $item['FK_product_id'] . ' P: ' . $product_info[$item['FK_product_id'] - 1]['price'] . '<br>';
-        
-        $subtotal += $item['quantity'] * $product_info[$item['FK_product_id'] - 1]['price'];
-        
+// Fetch product information that are in cart
+$stmt = $pdo->prepare("SELECT product.* FROM item_in_cart, product WHERE product.product_id = item_in_cart.FK_product_id AND item_in_cart.FK_cart_id = '$cart_id'");
+$stmt->execute();
+$product_info = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Fetch customer's first name
+$stmt = $pdo->prepare("SELECT first_name FROM customer WHERE customer_id = '$user_id'");
+$stmt->execute();
+$first_name = $stmt->fetchColumn();
+
+// print_r($items_in_cart);
+// echo "<br>";
+// print_r($product_info);
+// print_r($first_name);
+
+$item_in_cart_count = 0;
+
+// Calculate subtotal and item count
+$subtotal = 0.00;
+foreach ($items_in_cart as $item) {
+    $item_info = [];
+    foreach($product_info as $info) {
+        if ($info['product_id'] === $item['FK_product_id']) {
+            $item_info = $info;
+        }
     }
 
-    echo 'Subtotal: ';
-    echo $subtotal;
+    // echo '<br>';
+    // echo 'Q: ' . $item['quantity'] . ' FK: '. $item['FK_product_id'] . ' P: ' . $item_info['price'] . '<br>';
+    
+    $subtotal += $item['quantity'] * $item_info['price'];
+    $item_in_cart_count += $item['quantity'];
+}
+// echo 'Subtotal: ' . $subtotal;
+// echo 'Item count: ' . $item_in_cart_count;
+
+if ($item_in_cart_count == 0 || $item_in_cart_count == 1) {
+    $item_in_cart_count_text = $item_in_cart_count .  ' item';
+}
+else {
+    $item_in_cart_count_text = $item_in_cart_count .  ' items';
+} 
+
+
+
 ?>
 
-<?=template_header('Shopping Cart')?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Shopping Cart</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="../js/cart.js"></script>
+        <link rel="stylesheet" href="../css/cart.css">
+    </head>
+
+    <header>
+        <!-- Navigation bar -->
+        <div class="menu-bar-container">
+            <div class="menu-bar-logo-wrapper">
+                <div class="menu-bar-logo">
+                    <img src="../resource/icon/logo.svg">
+                </div>
+            </div>
+            <div class="menu-bar-search-wrapper">
+                <form>
+                    <input type="text" placeholder="Search">
+                    <button type="submit">
+                        <img src="../resource/icon/search-icon.svg">
+                    </button>
+                </form>
+            </div>
+            <div class="menu-bar-user-wrapper">
+                <div class="user-info-wrapper">
+                    <div class="menu-bar-user-icon">
+                        <img src="../resource/icon/user-icon.svg">
+                    </div>
+                    <div class="user-name">
+                        <span><?=$first_name?></span>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-bar-cart-wrapper">
+                <div class="cart-info-wrapper">
+                    <div class="menu-bar-cart-icon">
+                        <img src="../resource/icon/cart-icon.svg">
+                    </div>
+                    <span>Cart:&nbsp;</span>
+                    <span id="cart-item-count-text"><?=$item_in_cart_count?></span>
+                </div>
+            </div>
+        </div>
+    </header>
 
 
-<table>
-    <thead>
-        <tr>
-            <td colspan="2">Item</td>
-            <td>Price</td>
-            <td>Quantity</td>
-            <td>Total</td>
-        </tr>
-    </thead>
-    <tbody>
-        <?php if (empty($items_in_cart)): ?>
-            <<tr>
-                <td colspan="5" style="text-align:center;">Your cart is empty!</td>
-            </tr>
-        <?php else: ?>
-            <?php foreach ($items_in_cart as $item): ?>
-                <tr>
-                    <td>
-                        <?php
-                            $img = !empty($product_info[$item['FK_product_id'] - 1]['image']) ? $product_info[$item['FK_product_id'] - 1]['image'] : '../resource/img/default/product_large.png';
-                            $img_id = !empty($product_info[$item['FK_product_id'] - 1]['image']) ? $product_info[$item['FK_product_id'] - 1]['product_name'] : 'Product image not available';
-                        ?>
-                        <img src="<?=$img?>" width="50" height="50" alt="<?=$img_id?>">
-                    </td>
-                    <td><?=$product_info[$item['FK_product_id'] - 1]['product_name']?></td>
-                    <td>$<?=$product_info[$item['FK_product_id'] - 1]['price']?></td>
-                    <td><?=$item['quantity']?></td>
-                    <td>
-                        <?php
-                            $item_total = $item['quantity'] * $product_info[$item['FK_product_id'] - 1]['price'];
-                        ?>
-                        $<?=$item_total?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        <?php endif; ?>
-            
-    </tbody>
-</table>
+    <body>
+        <div class="main-container">
+            <div class="main-cart-container">
+                <div class="cart-wrapper">
+                    <div class="item-row-wrapper cart-title-wrapper">
+                        <div class="product-img-wrapper"></div>
+                        <div class="product-name-wrapper product-name-title-wrapper">
+                            Item
+                        </div>
+                        <div class="product-price-wrapper">Price</div>
+                        <div class="product-quantity-wrapper">Quantity</div>
+                        <div class="product-total-wrapper">Total</div>
+                        <div class="product-remove-wrapper"></div>
+                    </div>
+                    <div class="empty-cart-msg-row-wrapper" id="empty-msg-row">
+                        <span>Your cart is empty.</span>
+                    </div>
+                    <?php
+                    foreach ($items_in_cart as $item):
+                        $id = $item['item_id'];
+                        $quantity = $item['quantity'];
+                        $item_info = [];
+                        foreach($product_info as $info) {
+                            if ($info['product_id'] === $item['FK_product_id']) {
+                                $item_info = $info;
+                                break;
+                            }
+                        }
+                        $image = $item_info['image'];
+                        $product_name = $item_info['product_name'];
+                        $price = $item_info['price'];
+                    ?>
+                    <div class="item-row-wrapper" id="item-row-<?=$id?>">
+                        <div class="product-img-wrapper" id="img-<?=$id?>">
+                            <?php
+                            $img = !empty($image) ? $image : '../resource/img/default/product_large.png';
+                            $img_id = !empty($image) ? $product_name : 'Product image not available';
+                            ?>
+                            <img src="<?=$img?>" alt="<?=$img_id?>">
+                        </div>
+                        <div class="product-name-wrapper">
+                            <span id="product-name">
+                                <?=$product_name?>
+                            </span>
+                        </div>
+                        <div class="product-price-wrapper">
+                            <span id="item-price-text-<?=$id?>">$<?=$price?></span>
+                        </div>
+                        <div class="product-quantity-wrapper">
+                            <!-- Decrement button -->
+                            <button class="decrement" data-id="<?=$id?>">
+                                &minus;
+                            </button>
+                            <!-- Item quantity -->
+                            <span id="quantity-text-<?=$id?>"><?=$quantity?></span>
+                            <!-- Increment button -->
+                            <button class="increment" data-id="<?=$id?>">
+                                &plus;
+                            </button>
+                        </div>
+                        <div class="product-total-wrapper">
+                            <?php
+                            $item_total = $quantity * $price;
+                            ?>
+                            <span id="item-total-text-<?=$id?>">$<?=$item_total?></span>
+                        </div>
+                        <!-- Remove button -->
+                        <div class="product-remove-wrapper">
+                            <button class="remove" data-id="<?=$id?>">
+                                Remove
+                            </button>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <div class="main-summary-container">
+                <div class="summary-wrapper">
+                    <div class="summary-title">Summary:</div>
+                    <div class="summary-item-count" id="summary-item-count-text">
+                        <?=$item_in_cart_count_text?>
+                    </div>
+                    <div class="summary-subtotal" id="subtotal-text">
+                        Subtotal: $<?=$subtotal?>
+                    </div>
+                    <div class="summary-check-out">
+                        <button id="check-out-btn">Check out</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 
-
-<?=template_footer()?>
+</html>
